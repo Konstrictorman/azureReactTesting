b6b7455997f1af673771be7d37b548083a64a4f8
Ricardo Álvarez <ralvarez@insolpr.com>
2022-08-27 22:55:59 -0500

    bullshit

--- src/authConfig.js
+++ src/authConfig.js
@@ -4,6 +4,7 @@ export const msalConfig = {
      authority: "https://login.microsoftonline.com/c9277e2f-d5b3-4cb1-8d3c-f374f42ffbc4", // This is a URL (e.g. https://login.microsoftonline.com/{your tenant ID})
      redirectUri: "https://zealous-coast-09027a810.1.azurestaticapps.net/",
    },
+
    cache: {
      cacheLocation: "sessionStorage", // This configures where your cache will be stored
      storeAuthStateInCookie: false, // Set this to "true" if you are having issues on IE11 or Edge
@@ -15,6 +16,7 @@ export const msalConfig = {
   //scopes: ["User.Read"]
   scopes: ["api://6cebbf4e-4092-418a-abc7-6d483c3f8f04/Fidelizacion_WEB"]
  };
+ 
  
  // Add the endpoints here for Microsoft Graph API services you'd like to use.
  export const graphConfig = {
