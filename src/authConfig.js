8a16e5d19fa3cb8dabe223884e91973b511c4435
Ricardo Álvarez <ralvarez@insolpr.com>
2022-08-27 21:57:09 -0500

    project setup

---
+++ src/authConfig.js
@@ -0,0 +1,22 @@
+export const msalConfig = {
+   auth: {
+     clientId: "6cebbf4e-4092-418a-abc7-6d483c3f8f04",
+     authority: "https://login.microsoftonline.com/e6eb211f-4f1d-4d85-a663-7ede2efbf39b", // This is a URL (e.g. https://login.microsoftonline.com/{your tenant ID})
+     redirectUri: "http://localhost:3000",
+   },
+   cache: {
+     cacheLocation: "sessionStorage", // This configures where your cache will be stored
+     storeAuthStateInCookie: false, // Set this to "true" if you are having issues on IE11 or Edge
+   }
+ };
+ 
+ // Add scopes here for ID token to be used at Microsoft identity platform endpoints.
+ export const loginRequest = {
+  //scopes: ["User.Read"]
+  scopes: ["api://6cebbf4e-4092-418a-abc7-6d483c3f8f04/Fidelizacion_WEB"]
+ };
+ 
+ // Add the endpoints here for Microsoft Graph API services you'd like to use.
+ export const graphConfig = {
+     graphMeEndpoint: "Enter_the_Graph_Endpoint_Here/v1.0/me"
+ };
